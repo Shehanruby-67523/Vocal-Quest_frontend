13aa9471e1cf1d1cdac202253c9f210a5dc08e4c
Sajani <sajani.siriwardhana29@gmail.com>
2026-05-17 18:04:07 +0530

    Created initial profile page structure

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,19 @@
+function Profile() {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+
+      <img
+        src="https://via.placeholder.com/150"
+        alt="profile"
+      />
+
+      <h2>Sajani</h2>
+      <p>sajani@email.com</p>
+
+      <button>Edit Profile</button>
+    </div>
+  );
+}
+
+export default Profile;
